7c112c07b084d5e8f40a5e01658b6ff579970496
Srividhya Varadhan <100423362+Srividhya0504@users.noreply.github.com>
2024-04-05 16:19:11 +0530

    Update LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -16,14 +16,6 @@ public class LoginController {
 
   @CrossOrigin(origins = "*")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-  LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
-      return new LoginResponse(user.token(secret));
-    } else {
-      throw new Unauthorized("Access Denied");
-    }
-  }
 }
 
 class LoginRequest implements Serializable {
